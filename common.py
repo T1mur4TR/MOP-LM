75487b97d8c2a2dfb6eca0ba93dee488a3bbc4df
Timur Akhmadullin <ATRTimur1@yandex.ru>
2025-03-28 23:01:05 +0300

    MHCA and RoPE

--- common.py
+++ common.py
@@ -16,12 +16,60 @@ class MultiheadSelfAttention(nn.Module):
         self.dropout = dropout
 
     def forward(self, x, attn_mask=None):
+        """
+        Args:
+            x (torch.Tensor): input tensor with shape [..., L, E]
+            attn_mask (Optional[torch.Tensor]): boolean (float) attention mask (bias) with shape [..., L, L] or [..., H, L, L]
+
+        Returns:
+            torch.Tensor: output tensor with shape [..., L, E]
+        """
         batch_shape = x.shape[:-2]
         L = x.shape[-2]
         q, k = self.qk(x).view(*batch_shape, L, self.num_heads, 2 * self.query_dim).transpose(-2, -3).chunk(2, dim=-1)
         v = self.v(x).view(*batch_shape, L, self.num_heads, self.value_dim).transpose(-2, -3)
 
-        if attn_mask is not None and attn_mask.dim() < x.dim(): # handle same mask for all heads
+        if attn_mask is not None and attn_mask.dim() <= x.dim(): # handle same mask for all heads
+            attn_mask = attn_mask.unsqueeze(-3)
+
+        out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, dropout_p=(self.dropout if self.training else 0.))
+
+        return self.out_proj(out.transpose(-2, -3).flatten(-2))
+    
+
+class MultiheadCrossAttention(nn.Module):
+    def __init__(self, embedding_dim, query_dim, value_dim, num_heads, context_embedding_dim=None, dropout=0.):
+        super().__init__()
+        if context_embedding_dim is None:
+            context_embedding_dim = embedding_dim
+        self.embedding_dim = embedding_dim
+        self.context_embedding_dim = context_embedding_dim
+        self.query_dim = query_dim
+        self.value_dim = value_dim
+        self.num_heads = num_heads
+        self.q = nn.Linear(embedding_dim, query_dim * num_heads)
+        self.k = nn.Linear(context_embedding_dim, query_dim * num_heads)
+        self.v = nn.Linear(context_embedding_dim, value_dim * num_heads)
+        self.out_proj = nn.Linear(value_dim * num_heads, embedding_dim)
+        self.dropout = dropout
+
+    def forward(self, x, y, attn_mask=None):
+        """
+        Args:
+            x (torch.Tensor): input tensor with shape [..., L, E]
+            y (torch.Tensor): context tensor with shape [..., S, Ec]
+            attn_mask (Optional[torch.Tensor]): boolean (float) attention mask (bias) with shape [..., L, S] or [..., H, L, S]
+
+        Returns:
+            torch.Tensor: output tensor with shape [..., L, E]
+        """
+        batch_shape = x.shape[:-2]
+        L = x.shape[-2]
+        q = self.q(x).view(*batch_shape, L, self.num_heads, self.query_dim).transpose(-2, -3)
+        k = self.k(y).view(*batch_shape, L, self.num_heads, self.query_dim).transpose(-2, -3)
+        v = self.v(y).view(*batch_shape, L, self.num_heads, self.value_dim).transpose(-2, -3)
+
+        if attn_mask is not None and attn_mask.dim() <= x.dim(): # handle same mask for all heads
             attn_mask = attn_mask.unsqueeze(-3)
 
         out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, dropout_p=(self.dropout if self.training else 0.))
@@ -32,6 +80,7 @@ class MultiheadSelfAttention(nn.Module):
 class T5MHSA(MultiheadSelfAttention):
     def __init__(self, bins, max_distance, embedding_dim, query_dim, value_dim, num_heads, dropout=0., causal=False):
         super().__init__(embedding_dim, query_dim, value_dim, num_heads, dropout)
+
         self.bins = bins
         self.max_distance = max_distance
         self.causal = causal
@@ -44,6 +93,14 @@ class T5MHSA(MultiheadSelfAttention):
         self.register_buffer("indices", indices, persistent=False)
 
     def forward(self, x, attn_mask=None):
+        """
+        Args:
+            x (torch.Tensor): input tensor with shape [..., L, E]
+            attn_mask (Optional[torch.Tensor]): boolean attention mask with shape [..., L, L]
+
+        Returns:
+            torch.Tensor: output tensor with shape [..., L, E]
+        """
         L = x.shape[-2]
 
         with pt.no_grad():
@@ -61,6 +118,65 @@ class T5MHSA(MultiheadSelfAttention):
         return super().forward(x, bias)
 
 
+class RoPE(nn.Module):
+    def __init__(self, dim, cache_size=512, base=10000):
+        super().__init__()
+
+        assert dim % 2 == 0
+
+        self.dim = dim
+        self.cache_size = cache_size
+        self.base = base
+
+        theta = self.base ** -(pt.arange(0, self.dim, 2) / self.dim)
+        self.register_buffer("theta", theta, persistent=False)
+
+        cache = self._compute_rotations(0, cache_size)
+        self.register_buffer("cache", cache, persistent=False)
+
+    def _compute_rotations(self, start_idx, end_idx):
+        angles = pt.arange(start_idx, end_idx, device=self.theta.device, dtype=self.theta.dtype).outer(self.theta)
+        return pt.stack([pt.cos(angles), pt.sin(angles)], dim=-1)
+    
+    def forward(self, x):
+        """
+        Args:
+            x (torch.Tensor): input tensor with shape [..., L, E]
+
+        Returns:
+            torch.Tensor: output tensor with shape [..., L, E]
+        """
+        L = x.shape[-2]
+
+        if L <= self.cache_size:
+            rotations = self.cache[:L]
+        else:
+            rotations = pt.cat([self.cache, self._compute_rotations(self.cache_size, L)])
+        
+        xshaped = x.reshape(*x.shape[:-1], -1, 2)
+
+        return pt.stack([
+            xshaped[..., 0] * rotations[..., 0] - xshaped[..., 1] * rotations[..., 1], 
+            xshaped[..., 1] * rotations[..., 0] + xshaped[..., 0] * rotations[..., 1]
+        ], dim=-1).flatten(-2)
+
+
 class RoPEMHSA(MultiheadSelfAttention):
-    pass
-    # TODO
+    def __init__(self, embedding_dim, query_dim, value_dim, num_heads, dropout=0, cache_size=512, base=10000):
+        super().__init__(embedding_dim, query_dim, value_dim, num_heads, dropout)
+
+        self.RoPE = RoPE(query_dim, cache_size, base)
+
+    def forward(self, x, attn_mask=None):
+        batch_shape = x.shape[:-2]
+        L = x.shape[-2]
+        qk = self.qk(x).view(*batch_shape, L, 2, self.num_heads, self.query_dim).transpose(-3, -4).contiguous()
+        q, k = self.RoPE(qk).view(*batch_shape, 2 * self.num_heads, L, self.query_dim).chunk(2, dim=-3)
+        v = self.v(x).view(*batch_shape, L, self.num_heads, self.value_dim).transpose(-2, -3)
+
+        if attn_mask is not None and attn_mask.dim() <= x.dim(): # handle same mask for all heads
+            attn_mask = attn_mask.unsqueeze(-3)
+
+        out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, dropout_p=(self.dropout if self.training else 0.))
+
+        return self.out_proj(out.transpose(-2, -3).flatten(-2))
